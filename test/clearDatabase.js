d041f7113c8274981721790a9be89f9a4a07d412
Chris Dibble <Chris.Dibble@inin.com>
2015-07-22 22:46:11 -0400

    Added interval data to random seed script.

--- test/clearDatabase.js
+++ test/clearDatabase.js
@@ -3,10 +3,13 @@ var MongoClient = require('mongodb').MongoClient;
 // Connection URL
 var url = process.env.MONGO_DB;//'mongodb://192.168.99.100:32769/hiredorfired';
 var userCollection = null;
+var intervalCollection = null;
 
 MongoClient.connect(url, function(err, db) {
-  console.log("Connected correctly to MongoDB");
-  userCollection = db.collection('users');
-  userCollection.removeMany();
-  db.close();
+    console.log("Connected correctly to MongoDB");
+    userCollection = db.collection('users');
+    userCollection.removeMany();
+    intervalCollection = db.collection('intervals');
+    intervalCollection.removeMany();
+    db.close();
 });
--- test/seedRandomData.js
+++ test/seedRandomData.js
@@ -23,30 +23,51 @@ function addUser(row, date){
 }
 
 var users = [];
+var intervals = [];
+var hiredList = [];
+var firedList = [];
 var index = 0
-var date = 1420070400000;
+var date = 1420070400000;// Jan 1, 2015
 for(var x=1; x<initialCount; x++){
     //add start users
     var user = addUser(array[x], date);
     users.push(user);
+    hiredList.push(user);
     index = x;
 }
 
+intervals.push({
+    date : date,
+    hired : hiredList,
+    fired : firedList
+});
+
 console.log(date)
+//1 run for each day until Sept 1, 2015
 while(date < 1441065600000 && index< 9998){
     date += 86400000
+    hiredList = [];
+    firedList = [];
 
     for(var x=0; x<random(0,7); x++){
         index++
         var user = addUser(array[index], date);
         users.push(user);
+        hiredList.push(user);
     }
 
     for(var x=0; x<random(0,2);x++){
         var removeIndex = random(initialCount + 1,users.length-1);
         //console.log('removing user at ' + removeIndex)
         users[removeIndex].removed = date;
+        firedList.push(users[removeIndex]);
     }
+
+    intervals.push({
+        date : date,
+        hired : hiredList,
+        fired : firedList
+    });
 }
 
 var MongoClient = require('mongodb').MongoClient;
@@ -55,16 +76,21 @@ var url = process.env.MONGO_DB;
 console.log("connecting to database");
 MongoClient.connect(url, function (err, db) {
 
-    userCollection = db.collection('users');
+    var userCollection = db.collection('users');
+    var intervalCollection = db.collection('intervals');
 
+    console.log("inserting users");
     for(var u in users){
-        console.log(users[u])
-            userCollection.insertOne(users[u])
+        console.log(users[u]);
+        userCollection.insertOne(users[u]);
     }
-
-    console.log("inserting users")
+    console.log("finished inserting users");
+    console.log("inserting intervals");
+    for(var i in intervals){
+        console.log(intervals[i]);
+        intervalCollection.insertOne(intervals[i]);
+    }
+    console.log("finished inserting intervals");
 
     db.close();
-
-
 });
