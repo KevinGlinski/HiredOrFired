295c2bb36b5ecd5af268d037cd8006cd08a8e27c
Chris Dibble <Chris.Dibble@inin.com>
2015-07-23 12:46:11 -0400

    Added test route to force processing of scheduled services.

--- core/routes/testRoutes.js
+++ core/routes/testRoutes.js
@@ -1,7 +1,14 @@
 var app = module.exports = require('express')();
 var logger = require('../../utils/traceLogger');
+var userStore = require('../userDataStore');
 
 app.post('/testEndpoint', function(req, res){
     logger.log("RECEIVED SCHEDULER RSPONSE: " + JSON.stringify(req.body));
+    res.sendStatus(200);
+});
+
+app.post('/forceTrigger', function(req, res){
+    logger.log("Forcing trigger");
+    userStore.updateCompleted();
     res.sendStatus(200);
 });
